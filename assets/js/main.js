c7e902f579f1168f6649e1280763679964a3e262
Andreas Grafen <andreas@grafen.info>
2023-10-04 14:50:27 +0200

    feat: changing blur colour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,27 @@
 ;(function (){
 
-  document.body.style.setProperty('--cursor-opacity', .8)
+  const socialCards   = document.querySelectorAll('.card')
+  const cursorWrapper = document.querySelector('.cursor-wrapper')
 
-  document.addEventListener(
-    'mousemove',
-    event => {
-      document.body.style.setProperty('--cursorX', `${event.clientX}px`)
-      document.body.style.setProperty('--cursorY', `${event.clientY}px`)
+  cursorWrapper.style.setProperty('--cursor-opacity', .8)
+
+  document.addEventListener('mousemove', event => {
+      cursorWrapper.style.setProperty('--cursorX', `${event.clientX}px`)
+      cursorWrapper.style.setProperty('--cursorY', `${event.clientY}px`)
       return
     }
   )
+
+  socialCards.forEach(card => {
+
+    card.addEventListener('mouseenter', () => {
+      const colour = card.style.getPropertyValue('--gradient-colour')
+      return cursorWrapper.style.setProperty('--cursor-colour', colour)
+    })
+
+    card.addEventListener('mouseleave', () => cursorWrapper.style.setProperty('--cursor-colour', 'var(--blue-raw)'))
+
+  })
+
 
 })()
